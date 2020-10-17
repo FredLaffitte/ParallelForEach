03d69013b578ca075132b4bf8c42da6e7052eeed
Frederico Laffitte <frederico.laffitte@supergasbrascombr.onmicrosoft.com>
2020-10-17 18:45:02 -0300

    Adicionar arquivos de projeto.

---
+++ ParallelForEach/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ParallelForEach
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] nums = Enumerable.Range(0, 10000000).ToArray();
+            CancellationTokenSource cts = new CancellationTokenSource();
+
+            // Use a instância do ParallelOptions para armazenar o CancellationToken
+            ParallelOptions po = new ParallelOptions();
+            po.CancellationToken = cts.Token;
+            po.MaxDegreeOfParallelism = System.Environment.ProcessorCount;
+            Console.WriteLine("Pressione qualquer tecla para iniciar. Pressione 'c' para cancelar.");
+            Console.ReadKey();
+
+            // Executando uma tarefa, pois assim nós podemos cancelar de uma outra thread.
+            Task.Factory.StartNew(() =>
+            {
+                if (Console.ReadKey().KeyChar == 'c')
+                    cts.Cancel();
+                Console.WriteLine("pressione qualquer tecla para sair");
+            });
+
+            try
+            {
+                Parallel.ForEach(nums, po, (num) =>
+                {
+                    double d = Math.Sqrt(num);
+                    Console.WriteLine("{0} em  {1}", d, Thread.CurrentThread.ManagedThreadId);
+                    //Gerando um ThrowException
+                    po.CancellationToken.ThrowIfCancellationRequested();
+                });
+            }
+            catch (OperationCanceledException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                cts.Dispose();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
